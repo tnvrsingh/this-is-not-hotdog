61e3d91fb9f449293433d0397b9c05e28980edd5
Tanvir Singh <tanvirsingh137@gmail.com>
2023-07-27 01:02:09 +0530

    got GPU to work! - windows is tough, but I'm too lazy to dual boot

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
+import os
 import tensorflow as tf
-tf.test.gpu_device_name()
+from tensorflow import keras
+from tensorflow.keras import layers
+print(tf.config.list_physical_devices('GPU'))
+
+print("Yoyii")
